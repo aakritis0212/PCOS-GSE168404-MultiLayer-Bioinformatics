03a45b082053c3032cdb285786d9c1d93d39be1c
AAkriti sharma <121090338+aakritis0212@users.noreply.github.com>
2026-09-11 22:16:59 +0530

    app.py

--- app.py
+++ app.py
@@ -22,10 +22,7 @@ def col(df, names):
     return next((x for x in names if x in df.columns), None)
 
 limma=load("PCOS_limma_results.csv")
-gsea = load("PCOS_significant_GSEA_pathways.csv")
-
-if gsea is None:
-    gsea = load("PCOS_GSEA_GO_BP_results.csv")
+gsea = load("PCOS_GSEA_GO_BP_results.csv")
 core=load("PCOS_core_genes_shared_pathways.csv")
 regs=load("PCOS_FINAL_REGULATOR_MASTER_TABLE.csv")
 sigregs=load("PCOS_FINAL_SIGNIFICANT_REGULATORS.csv")
